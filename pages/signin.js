2624f4df1d6b4b0c820a1fd7cf10b2bd4a49fead
Jonathan Steele <jonathan@steele.pro>
2020-04-29 14:28:23 +0100

    feat: Add signin, signup pages and associated forms
    Basic, unstyled for time being

---
+++ pages/signin.js
@@ -0,0 +1,42 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import { useAuthState, useAuthDispatch } from '../context/auth'
+import { useForm } from 'react-hook-form'
+
+function SignIn() {
+  const { signIn } = useAuthDispatch()
+  const { isAuthenticated } = useAuthState()
+  const methods = useForm()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (isAuthenticated) router.push('/')
+  }, [isAuthenticated])
+
+  const onSubmit = async (data) => {
+    try {
+      await signIn(data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  return (
+    <form onSubmit={methods.handleSubmit(onSubmit)}>
+      <input
+        type="text"
+        name="username"
+        ref={methods.register({ required: true })}
+      ></input>
+      <input
+        type="password"
+        name="password"
+        ref={methods.register({ required: true })}
+      ></input>
+      <button type="submit">Go</button>
+    </form>
+  )
+}
+
+export default SignIn
