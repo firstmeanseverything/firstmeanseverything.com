6600fb9cc129c0c2fe7577e2b2d85895e29ebf81
Jonathan Steele <jonathan@steele.pro>
2020-04-29 08:37:33 +0100

    feat: Add, initialise Userbase SDK

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
+import { useEffect } from 'react'
+import userbase from 'userbase-js'
+
 import '../styles/index.css'
 
 function App({ Component, pageProps }) {
+  useEffect(() => {
+    const initUserbase = async () => {
+      try {
+        await userbase.init({
+          appId: process.env.USERBASE_APP_ID,
+        })
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    initUserbase()
+  }, [])
+
   return <Component {...pageProps} />
 }
 
