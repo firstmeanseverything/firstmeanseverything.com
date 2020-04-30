70c33d4a4e23d011b735166a0575cb19048e8f66
Jonathan Steele <jonathan@steele.pro>
2020-04-30 08:07:24 +0100

    chore: Add graphql-request, swr dependencies

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 module.exports = {
   env: {
+    GRAPHCMS_ENDPOINT: process.env.GRAPHCMS_ENDPOINT,
+    GRAPHCMS_TOKEN: process.env.GRAPHCMS_TOKEN,
     USERBASE_APP_ID: process.env.USERBASE_APP_ID,
   },
 }
