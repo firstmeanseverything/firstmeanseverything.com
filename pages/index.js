53e004b21d724cb18d4449d513dec7db4a51bfc5
Jonathan Steele <jonathan@steele.pro>
2020-07-14 15:35:59 +0100

    feat: Fetch prices from Stripe

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
+import Stripe from 'stripe'
+
 import ProgramList from '../components/program-list'
 
-function Index() {
+function Index({ prices }) {
   return <ProgramList />
+}
+
+export async function getStaticProps() {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
+  const { data: prices } = await stripe.prices.list({
+    active: true,
+    expand: ['data.product'],
+    product: 'prod_GluoMMJud0rQns',
+  })
+
+  return {
+    props: { prices },
+  }
 }
 
 export default Index
