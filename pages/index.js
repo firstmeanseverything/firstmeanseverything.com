0de413c1a4155846db673d2e5d50537962b5f18e
Jonathan Steele <jonathan@steele.pro>
2020-08-04 11:30:46 +0100

    style: Remove log

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,7 @@ function Index() {
     (query, category, date) => graphcms.request(query, { category, date }),
     { revalidateOnFocus: false }
   )
-  console.log(user)
+
   return (
     <div className="flex flex-col">
       <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
