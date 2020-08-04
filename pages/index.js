fdd54e2412ac4e408a6e6225d7d4073aa97a517f
Jonathan Steele <jonathan@steele.pro>
2020-08-04 11:28:31 +0100

    feat: Table view for programming

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,133 @@
-import Stripe from 'stripe'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { GraphQLClient } from 'graphql-request'
+import useSWR from 'swr'
+import cx from 'classnames'
 
-import { createCheckoutSession, goToBillingPortal } from '../lib/db'
+import DocDownloadSVG from '../svgs/document-download.svg'
 import { useAuthState } from '../context/auth'
 
-function Index({ prices }) {
-  const { user } = useAuthState()
+function Index() {
+  const { isAuthenticating, user } = useAuthState()
+  const router = useRouter()
+  const [category, setCategory] = useState('RX')
 
-  return (
-    <React.Fragment>
-      <div>
-        {prices.map((price) =>
-          new Intl.NumberFormat('en-US', {
-            style: 'currency',
-            currency: price.currency,
-            minimumFractionDigits: 0,
-          }).format(price.unit_amount / 100)
-        )}
-      </div>
-      <button onClick={() => createCheckoutSession(user.uid, prices[0].id)}>
-        Subscribe
-      </button>
-      <button onClick={() => goToBillingPortal()}>Manage</button>
-    </React.Fragment>
+  useEffect(() => {
+    if (!isAuthenticating && !user) router.push('/signin')
+  }, [isAuthenticating, user])
+
+  const graphcms = new GraphQLClient(
+    process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT,
+    {
+      headers: {
+        authorization: `Bearer ${process.env.NEXT_PUBLIC_GRAPHCMS_TOKEN}`,
+      },
+    }
   )
-}
 
-export async function getStaticProps() {
-  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  const date = new Date().toDateString()
 
-  const { data: prices } = await stripe.prices.list({
-    active: true,
-    expand: ['data.product'],
-    product: 'prod_HhE1ef4MH5DDwC',
-  })
+  const { data, error } = useSWR(
+    user
+      ? [
+          `query AvailablePrograms($category: ProgramCategory!, $date: Date!) {
+            programs(where: {date_lt: $date, category: $category}) {
+              date
+              category
+              id
+              pdf {
+                url
+              }
+            }
+          }`,
+          category,
+          date,
+        ]
+      : null,
+    (query, category, date) => graphcms.request(query, { category, date }),
+    { revalidateOnFocus: false }
+  )
+  console.log(user)
+  return (
+    <div className="flex flex-col">
+      <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+        <div className="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead>
+              <tr>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Date
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                  Category
+                </th>
+                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {!data ? (
+                <tr>
+                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                </tr>
+              ) : (
+                data.programs.map((program) => {
+                  const dateDiff = Math.floor(
+                    (new Date() - new Date(program.date)) /
+                      (1000 * 60 * 60 * 24)
+                  )
 
-  return {
-    props: { prices },
-  }
+                  const isNew = dateDiff <= 100
+
+                  return (
+                    <tr key={program.id}>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <div className="flex items-center">
+                          {isNew && (
+                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
+                              New
+                            </span>
+                          )}
+                          <div className={cx({ 'ml-4': isNew })}>
+                            <div className="text-sm leading-5 font-medium text-gray-900">
+                              {new Intl.DateTimeFormat('en-GB', {
+                                weekday: 'long',
+                                year: 'numeric',
+                                month: 'long',
+                                day: 'numeric',
+                              }).format(new Date(program.date))}
+                            </div>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                          {program.category}
+                        </span>
+                      </td>
+
+                      <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                        <a
+                          className="text-indigo-600 hover:text-indigo-900"
+                          href={program.pdf.url}
+                          target="_blank"
+                          rel="nofollow noreferrer"
+                          title="Download PDF"
+                        >
+                          <DocDownloadSVG className="h-6 w-auto" />
+                        </a>
+                      </td>
+                    </tr>
+                  )
+                })
+              )}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Index
