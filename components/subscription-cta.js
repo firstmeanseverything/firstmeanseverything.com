960af93f9e29d1ad9f890c5f21a9dce24544729a
Jonathan Steele <jonathan@steele.pro>
2020-08-11 11:49:30 +0100

    feat: Enable subscription purchasing (#43)
    * chore(deps): Add firebase-admin  dependency
    
    * feat: Initialise Firebase Admin
    
    * feat: Fetch Stripe product from Firebase
    
    * feat: Pricing CTA, with subscription functionality
    
    * style: Remove log
    
    * fix: Parse Firebase private_key variable

---
+++ components/subscription-cta.js
@@ -0,0 +1,153 @@
+import { useState } from 'react'
+import cx from 'classnames'
+
+import { createCheckoutSession } from '../lib/db'
+import { useAuthState } from '../context/auth'
+
+function SubscriptionCTA({ description, name, prices }) {
+  const { user } = useAuthState()
+  const [activeBillingInterval, setBillingInterval] = useState('month')
+  const [checkoutLoading, setCheckoutLoading] = useState(false)
+
+  const activePrice = prices.find(
+    (price) => price.interval === activeBillingInterval
+  )
+
+  return (
+    <div className="pb-16 sm:pb-20 lg:pb-28">
+      <div className="max-w-lg mx-auto rounded-lg shadow-sm border border-gray-200 overflow-hidden lg:max-w-none lg:flex">
+        <div className="bg-white px-6 py-8 lg:flex-shrink-1 lg:p-12">
+          <h3 className="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
+            {name}
+          </h3>
+          <p className="mt-6 text-base leading-6 text-gray-500">
+            {description}
+          </p>
+          <div className="mt-8">
+            <div className="flex items-center">
+              <h4 className="flex-shrink-0 pr-4 bg-white text-sm leading-5 tracking-wider font-semibold uppercase text-indigo-600">
+                What's included
+              </h4>
+              <div className="flex-1 border-t-2 border-gray-200"></div>
+            </div>
+            <ul className="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
+              <li className="flex items-start lg:col-span-1">
+                <div className="flex-shrink-0">
+                  <svg
+                    className="h-5 w-5 text-green-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-3 text-sm leading-5 text-gray-700">
+                  Weekly program published every Monday
+                </p>
+              </li>
+              <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                <div className="flex-shrink-0">
+                  <svg
+                    className="h-5 w-5 text-green-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-3 text-sm leading-5 text-gray-700">
+                  RX and scaled tracks available
+                </p>
+              </li>
+              <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                <div className="flex-shrink-0">
+                  <svg
+                    className="h-5 w-5 text-green-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-3 text-sm leading-5 text-gray-700">
+                  8 week cycles switching focus between conditioning,
+                  weightlifting and gymnastics
+                </p>
+              </li>
+              <li className="mt-5 flex items-start lg:col-span-1 lg:mt-0">
+                <div className="flex-shrink-0">
+                  <svg
+                    className="h-5 w-5 text-green-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+                <p className="ml-3 text-sm leading-5 text-gray-700">
+                  Access to private Facebook community
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="py-8 px-6 text-center bg-gray-800 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
+          <p className="text-lg leading-6 font-medium text-gray-300">
+            Subscribe today for just
+          </p>
+          <div className="mt-4 flex items-center justify-center text-5xl leading-none font-extrabold text-white">
+            <span>
+              {new Intl.NumberFormat('en-GB', {
+                style: 'currency',
+                currency: activePrice.currency,
+                maximumFractionDigits: 0,
+                minimumFractionDigits: 0,
+              }).format(activePrice.unit_amount / 100)}
+            </span>
+            <span className="ml-3 text-xl leading-7 font-medium text-gray-300">
+              / {activePrice.interval}
+            </span>
+          </div>
+          <div className="mt-6">
+            <div className="rounded-md shadow">
+              <button
+                onClick={() => {
+                  setCheckoutLoading(true)
+                  createCheckoutSession(user.uid, activePrice.id)
+                }}
+                className={cx(
+                  'w-full flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-gray-700 bg-yellow-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out',
+                  {
+                    'cursor-not-allowed opacity-50': checkoutLoading,
+                    'hover:bg-yellow-200': !checkoutLoading,
+                  }
+                )}
+                disabled={checkoutLoading}
+              >
+                Unlock
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SubscriptionCTA
--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,11 @@ import useSWR from 'swr'
 import cx from 'classnames'
 
 import { graphcmsClient } from '../lib/graphcms'
+import { getProduct } from '../lib/db-admin'
+import SubscriptionCTA from '../components/subscription-cta'
 import { useAuthState } from '../context/auth'
 
-function Index() {
+function Index({ product }) {
   const { isAuthenticating, user } = useAuthState()
   const router = useRouter()
   const [activeCategory, setActiveCategory] = useState('RX')
@@ -61,133 +63,146 @@ function Index() {
   ]
 
   return (
-    <div className="flex flex-col">
-      <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-        <div className="align-middle inline-block min-w-full overflow-hidden border border-gray-200 rounded shadow-sm">
-          <div className="bg-white px-4 py-3 flex items-center justify-between sm:px-6">
-            <nav className="flex space-x-4">
-              {programCategories.map((category, index) => (
-                <button
-                  onClick={() => setActiveCategory(category.value)}
-                  className={cx(
-                    'px-3 py-2 font-medium text-sm leading-5 rounded-md focus:outline-none',
-                    {
-                      'text-indigo-700 bg-indigo-100 focus:text-indigo-800 focus:bg-indigo-200':
-                        activeCategory === category.value,
-                      'text-gray-500 hover:text-gray-700 focus:text-indigo-600 focus:bg-indigo-50':
-                        activeCategory !== category.value,
+    <React.Fragment>
+      {user && !hasSubscription && <SubscriptionCTA {...product} />}
+      <div className="flex flex-col">
+        <div className="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+          <div className="align-middle inline-block min-w-full overflow-hidden border border-gray-200 rounded-lg shadow-sm">
+            <div className="bg-white px-4 py-3 flex items-center justify-between sm:px-6">
+              <nav className="flex space-x-4">
+                {programCategories.map((category, index) => (
+                  <button
+                    onClick={() => setActiveCategory(category.value)}
+                    className={cx(
+                      'px-3 py-2 font-medium text-sm leading-5 rounded-md focus:outline-none',
+                      {
+                        'text-indigo-700 bg-indigo-100 focus:text-indigo-800 focus:bg-indigo-200':
+                          activeCategory === category.value,
+                        'text-gray-500 hover:text-gray-700 focus:text-indigo-600 focus:bg-indigo-50':
+                          activeCategory !== category.value,
+                      }
+                    )}
+                    aria-current={
+                      activeCategory === category.value ? 'page' : 'false'
                     }
-                  )}
-                  aria-current={
-                    activeCategory === category.value ? 'page' : 'false'
-                  }
-                  key={index}
-                >
-                  {category.label}
-                </button>
-              ))}
-            </nav>
-          </div>
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead>
-              <tr className="border-t border-gray-200">
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  {hasSubscription ? 'Date' : 'Title'}
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                  Category
-                </th>
-                <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {!data ? (
-                <tr>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
-                  <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                    key={index}
+                  >
+                    {category.label}
+                  </button>
+                ))}
+              </nav>
+            </div>
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead>
+                <tr className="border-t border-gray-200">
+                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    {hasSubscription ? 'Date' : 'Title'}
+                  </th>
+                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
+                    Category
+                  </th>
+                  <th className="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
                 </tr>
-              ) : (
-                data.programs.map((program, index) => {
-                  const dateDiff = Math.floor(
-                    (new Date() - new Date(program.date)) /
-                      (1000 * 60 * 60 * 24)
-                  )
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {!data ? (
+                  <tr>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                    <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200"></td>
+                  </tr>
+                ) : (
+                  data.programs.map((program, index) => {
+                    const dateDiff = Math.floor(
+                      (new Date() - new Date(program.date)) /
+                        (1000 * 60 * 60 * 24)
+                    )
 
-                  const isNew = dateDiff <= 100
+                    const isNew = dateDiff <= 7
 
-                  return (
-                    <tr key={program.id}>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        <div className="flex items-center">
-                          {!hasSubscription && (
-                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
-                              Free
-                            </span>
-                          )}
-                          {isNew && (
-                            <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
-                              New
-                            </span>
-                          )}
-                          <div
-                            className={cx({
-                              'ml-4': isNew || !hasSubscription,
-                            })}
-                          >
-                            <div className="text-sm leading-5 font-medium text-gray-900">
-                              {hasSubscription
-                                ? new Intl.DateTimeFormat('en-GB', {
-                                    weekday: 'long',
-                                    year: 'numeric',
-                                    month: 'long',
-                                    day: 'numeric',
-                                  }).format(new Date(program.date))
-                                : `Sample week ${index + 1}`}
+                    return (
+                      <tr key={program.id}>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                          <div className="flex items-center">
+                            {!hasSubscription && (
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
+                                Free
+                              </span>
+                            )}
+                            {isNew && (
+                              <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800">
+                                New
+                              </span>
+                            )}
+                            <div
+                              className={cx({
+                                'ml-4': isNew || !hasSubscription,
+                              })}
+                            >
+                              <div className="text-sm leading-5 font-medium text-gray-900">
+                                {hasSubscription
+                                  ? new Intl.DateTimeFormat('en-GB', {
+                                      weekday: 'long',
+                                      year: 'numeric',
+                                      month: 'long',
+                                      day: 'numeric',
+                                    }).format(new Date(program.date))
+                                  : `Sample week ${index + 1}`}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          {program.category}
-                        </span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                        <Link
-                          href={
-                            hasSubscription
-                              ? '/program/[category]/[date]'
-                              : '/program/[category]/sample/[id]'
-                          }
-                          as={
-                            hasSubscription
-                              ? `/program/${program.category.toLowerCase()}/${
-                                  program.date
-                                }`
-                              : `/program/${program.category.toLowerCase()}/sample/${
-                                  program.id
-                                }`
-                          }
-                        >
-                          <a
-                            className="text-indigo-600 hover:text-indigo-900"
-                            title="View"
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                            {program.category}
+                          </span>
+                        </td>
+                        <td className="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
+                          <Link
+                            href={
+                              hasSubscription
+                                ? '/program/[category]/[date]'
+                                : '/program/[category]/sample/[id]'
+                            }
+                            as={
+                              hasSubscription
+                                ? `/program/${program.category.toLowerCase()}/${
+                                    program.date
+                                  }`
+                                : `/program/${program.category.toLowerCase()}/sample/${
+                                    program.id
+                                  }`
+                            }
                           >
-                            View
-                          </a>
-                        </Link>
-                      </td>
-                    </tr>
-                  )
-                })
-              )}
-            </tbody>
-          </table>
+                            <a
+                              className="text-indigo-600 hover:text-indigo-900"
+                              title="View"
+                            >
+                              View
+                            </a>
+                          </Link>
+                        </td>
+                      </tr>
+                    )
+                  })
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
-    </div>
+    </React.Fragment>
   )
+}
+
+export async function getStaticProps() {
+  const { product } = await getProduct(process.env.STRIPE_PRODUCT_ID)
+
+  return {
+    props: {
+      product,
+    },
+  }
 }
 
 export default Index
