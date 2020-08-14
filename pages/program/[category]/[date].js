6b08bd34462c0026053ee5c30ff04899a1c9f427
Jonathan Steele <jonathan@steele.pro>
2020-08-14 14:25:33 +0100

    feat: Basic program page layout

--- pages/program/[category]/[date].js
+++ pages/program/[category]/[date].js
@@ -4,9 +4,8 @@ import renderToString from 'next-mdx-remote/render-to-string'
 import hydrate from 'next-mdx-remote/hydrate'
 
 import { graphcmsClient } from '../../../lib/graphcms'
+import Page from '../../../components/page'
 import { useAuthState } from '../../../context/auth'
-
-const components = { h1: (props) => <h1 {...props} /> }
 
 function ProgramPage({ program }) {
   const { user } = useAuthState()
@@ -16,9 +15,61 @@ function ProgramPage({ program }) {
     if (!user || user.stripeRole !== 'basic') router.push('/')
   }, [user])
 
-  const content = hydrate(program.mdx, components)
+  const content = hydrate(program.mdx)
 
-  return content
+  const ProgramMeta = ({ bias, category, date }) => {
+    return (
+      <React.Fragment>
+        <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
+          <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+            {category}
+          </span>
+        </div>
+        <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
+          <svg
+            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+              clipRule="evenodd"
+            />
+          </svg>
+          {new Intl.DateTimeFormat('en-GB', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          }).format(new Date(date))}
+        </div>
+        <div class="mt-2 flex items-center text-sm leading-5 text-gray-500">
+          <svg
+            class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+              clipRule="evenodd"
+            />
+          </svg>
+          {bias}
+        </div>
+      </React.Fragment>
+    )
+  }
+
+  return (
+    <Page title={program.title} meta={<ProgramMeta {...program} />}>
+      <div className="bg-white shadow rounded sm:rounded-lg">
+        <div className="px-4 py-5 sm:p-6">
+          <div className="prose">{content}</div>
+        </div>
+      </div>
+    </Page>
+  )
 }
 
 export async function getStaticPaths() {
@@ -51,13 +102,15 @@ export async function getStaticProps({ params }) {
     `
     query ProgramPageQuery($date: Date!, $category: ProgramCategory!) {
       programs(where: { date: $date, category: $category }) {
+        bias
+        category
         content {
           markdown
         }
         date
-        category
         free
         id
+        title
       }
     }`,
     {
@@ -69,7 +122,7 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       program: {
-        mdx: await renderToString(program.content.markdown, components),
+        mdx: await renderToString(program.content.markdown),
         ...program,
       },
     },
