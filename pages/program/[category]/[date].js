45ac04a9c9c0592ce587cc30120f23499a59a012
Jonathan Steele <jonathan@steele.pro>
2020-08-15 12:34:52 +0100

    feat: Accordion for program days

--- pages/program/[category]/[date].js
+++ pages/program/[category]/[date].js
@@ -1,7 +1,8 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import renderToString from 'next-mdx-remote/render-to-string'
 import hydrate from 'next-mdx-remote/hydrate'
+import cx from 'classnames'
 import he from 'he'
 
 import { graphcmsClient } from '../../../lib/graphcms'
@@ -16,7 +17,51 @@ function ProgramPage({ program }) {
     if (!user || user.stripeRole !== 'basic') router.push('/')
   }, [user])
 
-  const content = hydrate(program.mdx)
+  const DaySection = ({ children, title }) => {
+    const [dayOpen, setDayOpen] = useState(false)
+
+    const toggleDayOpen = () => setDayOpen((open) => !open)
+
+    return (
+      <div className="pt-6 first:pt-0">
+        <dt className="text-2xl leading-7">
+          <button
+            onClick={() => toggleDayOpen()}
+            className="text-left w-full flex justify-between items-start text-gray-400 focus:outline-none focus:text-gray-900"
+          >
+            <span className="font-medium text-gray-900">{title}</span>
+            <span className="ml-6 h-7 flex items-center">
+              <svg
+                className={cx('h-6 w-6 transform', {
+                  '-rotate-180': dayOpen,
+                  'rotate-0': !dayOpen,
+                })}
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                />
+              </svg>
+            </span>
+          </button>
+        </dt>
+        <dd
+          className={cx('border-t mt-6 pr-12 pt-6', {
+            hidden: !dayOpen,
+          })}
+        >
+          <div className="prose">{children}</div>
+        </dd>
+      </div>
+    )
+  }
+
+  const content = hydrate(program.mdx, { DaySection })
 
   const ProgramMeta = ({ bias, category, date }) => {
     return (
@@ -66,7 +111,7 @@ function ProgramPage({ program }) {
     <Page title={program.title} meta={<ProgramMeta {...program} />}>
       <div className="bg-white shadow rounded sm:rounded-lg">
         <div className="px-4 py-5 sm:p-6">
-          <div className="prose">{content}</div>
+          <dl className="divide-y space-y-6">{content}</dl>
         </div>
       </div>
     </Page>
