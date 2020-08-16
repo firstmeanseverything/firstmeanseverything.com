45d961c739b81402ae4370070f655f4a71792429
Jonathan Steele <jonathan@steele.pro>
2020-08-16 14:52:14 +0100

    style: Remove unused import

--- pages/program/[category]/[date].js
+++ pages/program/[category]/[date].js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import renderToString from 'next-mdx-remote/render-to-string'
 import he from 'he'
