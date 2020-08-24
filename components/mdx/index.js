dd6269aeda37b146be27ac812637380be1ed2ae2
Jonathan Steele <jonathan@steele.pro>
2020-08-24 11:57:06 +0100

    feat: Add <PercentageWeightWork /> component

--- components/mdx/index.js
+++ components/mdx/index.js
@@ -1,3 +1,4 @@
+import { useAuthState } from '../../context/auth'
 import WorkoutBlock from '../workout-block'
 
 const mdxComponents = {
@@ -11,6 +12,35 @@ const mdxComponents = {
       {children}
     </WorkoutBlock>
   ),
+  PercentageWeightWork: ({ movement, note, percent, scheme }) => {
+    const { user } = useAuthState()
+
+    const userWeight = user?.[movement]
+      ? `${user[movement] * Number(percent / 100)}kg`
+      : `${percent}%`
+
+    const parseMovement = (movement) => {
+      switch (true) {
+        case movement.includes('back_squat'):
+          return 'Back squat'
+        case movement.includes('deadlift'):
+          return 'Deadlift'
+        case movement.includes('front_squat'):
+          return 'Front squat'
+        case movement.includes('power_clean'):
+          return 'Power clean'
+        case movement.includes('squat_clean'):
+          return 'Squat clean'
+      }
+    }
+
+    return (
+      <div>
+        <h4>{`${scheme} ${parseMovement(movement)} @ ${userWeight}`}</h4>
+        {note && <span className="text-sm">{note}</span>}
+      </div>
+    )
+  },
   Strength: ({ children, ...props }) => (
     <WorkoutBlock {...props} title="Strength">
       {children}
