6e7f16109150617b3c07f81b5e3e2e41f70ddd0c
Jonathan Steele <jonathan@steele.pro>
2020-08-24 12:41:27 +0100

    feat: Add <PercentageRepWork /> component

--- components/mdx/index.js
+++ components/mdx/index.js
@@ -12,6 +12,30 @@ const mdxComponents = {
       {children}
     </WorkoutBlock>
   ),
+  PercentageRepWork: ({ movement, note, percent, scheme }) => {
+    const { user } = useAuthState()
+
+    const parseRepCount =
+      Number(percent) >= 100
+        ? Number(percent / 100) * Number(user?.[movement])
+        : (Number(user?.[movement]) * percent) / 100
+
+    const userReps = user?.[movement] ? parseRepCount : `${Number(percent)}%`
+
+    const parseMovement = (movement) => {
+      switch (true) {
+        case movement.includes('toes_to_bar'):
+          return 'Toes to bar'
+      }
+    }
+
+    return (
+      <div>
+        <h4>{`${scheme} ${userReps} ${parseMovement(movement)}`}</h4>
+        {note && <span className="text-sm">{note}</span>}
+      </div>
+    )
+  },
   PercentageWeightWork: ({ movement, note, percent, scheme }) => {
     const { user } = useAuthState()
 
