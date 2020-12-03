1a32bcbc8c8dd5a41a3be6a2a1ee04cfa70f2e0c
Jonathan Steele <jonathan@steele.pro>
2020-12-03 21:12:40 +0000

    feat: Fetch products from GraphCMS

---
+++ pages/store/index.js
@@ -0,0 +1,55 @@
+import * as React from 'react'
+import useSWR from 'swr'
+import { gql } from 'graphql-request'
+
+import { graphcmsClient } from '@lib/graphcms'
+
+function Store({ categories }) {
+  const [category, setCategory] = React.useState('Sweatshirts')
+  const { data, error } = useSWR(
+    [
+      gql`
+        query ProductQuery($category: String) {
+          products(where: { categories_every: { name: $category } }) {
+            id
+            images {
+              id
+              height
+              url
+              width
+            }
+            name
+            slug
+          }
+        }
+      `,
+      category
+    ],
+    (query, category) => graphcmsClient.request(query, { category })
+  )
+
+  return data
+    ? data.products.map((product) => {
+        return <pre>{JSON.stringify(product, null, 2)}</pre>
+      })
+    : 'Loading'
+}
+
+export async function getStaticProps() {
+  const { categories } = await graphcmsClient.request(gql`
+    {
+      categories {
+        id
+        name
+      }
+    }
+  `)
+
+  return {
+    props: {
+      categories
+    }
+  }
+}
+
+export default Store
