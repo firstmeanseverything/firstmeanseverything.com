7191c8fb6bcf1e9d5eaa4d06650e0951236d5a05
Jonathan Steele <steele@firstmeanseverything.com>
2021-04-14 22:17:38 +0100

    feat: Add transition to program day panels (#268)
    * feat: Remove rest day from program UI
    
    * chore(deps): Bump @headlessui/react dependency
    
    * feat: Transition day section opening, closing with <Disclosure />

--- components/day-section.js
+++ components/day-section.js
@@ -1,18 +1,14 @@
 import * as React from 'react'
 import hydrate from 'next-mdx-remote/hydrate'
 import cx from 'classnames'
+import { Disclosure, Transition } from '@headlessui/react'
 import { ChevronDownIcon } from '@heroicons/react/outline'
 
 import { AuthProvider } from '@/context/auth'
-import Badge from './badge'
 import mdxComponents from './mdx'
 import WorkoutBlock from './workout-block'
 
-function DaySection({ activeRecovery = false, content, rest = false, title }) {
-  const [dayOpen, setDayOpen] = React.useState(false)
-
-  const toggleDayOpen = () => setDayOpen((open) => !open)
-
+function DaySection({ activeRecovery = false, content, title }, index) {
   const mdxContent = content
     ? hydrate(content.mdx, {
         components: mdxComponents,
@@ -21,56 +17,51 @@ function DaySection({ activeRecovery = false, content, rest = false, title }) {
     : null
 
   return (
-    <div key={title}>
-      <dt className="px-4 py-4 text-lg leading-7 sm:py-5 sm:px-6">
-        <button
-          {...(!rest && { onClick: () => toggleDayOpen() })}
-          className={cx(
-            'text-left w-full flex justify-between items-center text-gray-400 focus:outline-none focus:text-gray-900',
-            {
-              'cursor-default': rest
-            }
-          )}
-        >
-          <span className="font-medium text-gray-900">{title}</span>
-          {rest ? (
-            <Badge label="Rest" theme="green" />
-          ) : (
-            <span className="ml-6 h-7 flex items-center">
-              <ChevronDownIcon
-                className={cx(
-                  'h-6 w-6 transform transition-transform duration-100',
-                  {
-                    '-rotate-180': dayOpen,
-                    'rotate-0': !dayOpen
-                  }
+    <Disclosure key={index} as="div">
+      {({ open }) => (
+        <React.Fragment>
+          <dt className="px-4 py-4 text-lg leading-7 sm:py-5 sm:px-6">
+            <Disclosure.Button className="text-left w-full flex justify-between items-center text-gray-400 focus:outline-none focus:text-gray-900">
+              <span className="font-medium text-gray-900">{title}</span>
+
+              <span className="ml-6 h-7 flex items-center">
+                <ChevronDownIcon
+                  className={cx(
+                    'h-6 w-6 transform transition-transform duration-100',
+                    open ? '-rotate-180' : 'rotate-0'
+                  )}
+                />
+              </span>
+            </Disclosure.Button>
+          </dt>
+          <Transition
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Disclosure.Panel
+              static
+              as="dd"
+              className="border-b border-t border-gray-200 bg-gray-50 px-4 py-6 sm:border-b-0 sm:px-6 sm:py-8"
+            >
+              <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
+                {activeRecovery ? (
+                  <WorkoutBlock title="Active Recovery" type="recovery">
+                    {mdxContent}
+                  </WorkoutBlock>
+                ) : (
+                  mdxContent
                 )}
-              />
-            </span>
-          )}
-        </button>
-      </dt>
-      {!rest && (
-        <div
-          className={cx(
-            'border-b border-t border-gray-200 bg-gray-50 px-4 py-6 sm:border-b-0 sm:px-6 sm:py-8',
-            {
-              hidden: !dayOpen
-            }
-          )}
-        >
-          <ul className="mt-3 grid grid-cols-1 gap-5 sm:gap-6">
-            {activeRecovery ? (
-              <WorkoutBlock title="Active Recovery" type="recovery">
-                {mdxContent}
-              </WorkoutBlock>
-            ) : (
-              mdxContent
-            )}
-          </ul>
-        </div>
+              </ul>
+            </Disclosure.Panel>
+          </Transition>
+        </React.Fragment>
       )}
-    </div>
+    </Disclosure>
   )
 }
 
