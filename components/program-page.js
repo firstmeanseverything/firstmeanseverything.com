fbe0cfb0bad5043ab45bac8186bb1926ef605740
Jonathan Steele <jonathan@steele.pro>
2021-04-14 14:50:25 +0100

    fix: Use router.back() to preserve query state

--- components/program-page.js
+++ components/program-page.js
@@ -36,7 +36,7 @@ function ProgramPage({ program }) {
         <div className="mt-6 flex flex-col-reverse justify-stretch space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-x-reverse sm:space-y-0 sm:space-x-3 md:mt-0 md:flex-row md:space-x-3">
           <button
             type="button"
-            onClick={() => router.push('/')}
+            onClick={() => router.back()}
             className="inline-flex items-center justify-center px-4 py-2 border border-gray-300 shadow-sm text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-blue-500"
           >
             <ArrowNarrowLeftIcon
