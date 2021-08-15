8ecec85712922187085fcd3745c6263eb6fc465c
Jonathan Steele <jonathan@steele.pro>
2021-08-15 11:51:42 +0100

    fix: Prevent recreating `stripe` constant on every render

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,9 @@ import Layout from '@/components/layout'
 import 'tailwindcss/tailwind.css'
 import { defaultSeo } from 'next-seo.config'
 
-function App({ Component, pageProps }) {
-  const stripePromise = loadStripe(
-    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-  )
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 
+function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => <Layout>{page}</Layout>)
 
   return (
