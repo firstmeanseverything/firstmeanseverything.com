2c7ca85589257f5e13044ca6ba6645b8a181ed9d
Jonathan Steele <jonathan@steele.pro>
2022-09-06 21:30:47 +0100

    fix: Prevent duplicate program listings

--- graphql/queries/program.js
+++ graphql/queries/program.js
@@ -25,6 +25,7 @@ const ProgramPreviewPageQuery = gql`
 
 const ProgramsListQuery = gql`
   query ProgramsListQuery(
+    $category: ProgramCategory!
     $from: Date!
     $limit: Int!
     $offset: Int!
@@ -35,7 +36,12 @@ const ProgramsListQuery = gql`
       orderBy: date_DESC
       skip: $offset
       stage: $stage
-      where: { OR: [{ date_gte: $from }, { test: true }] }
+      where: {
+        OR: [
+          { category: $category, date_gte: $from }
+          { category: $category, test: true }
+        ]
+      }
     ) {
       ...ProgramListFragment
     }
