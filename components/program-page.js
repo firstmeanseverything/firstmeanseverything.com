df26c37d761c9d43e38822247017dbb6fec2e194
Jonathan Steele <jonathan@steele.pro>
2022-09-06 20:25:23 +0100

    feat: Remove 'back' button

--- components/program-page.js
+++ components/program-page.js
@@ -1,6 +1,4 @@
 import * as React from 'react'
-import { useRouter } from 'next/router'
-import { ArrowNarrowLeftIcon } from '@heroicons/react/solid'
 
 import { APMarkSVG } from '@/svgs'
 import DaySection from '@/components/day-section'
@@ -8,8 +6,6 @@ import ProgramInfo from '@/components/program-info'
 import SEO from '@/components/seo'
 
 function ProgramPage({ program }) {
-  const router = useRouter()
-
   return (
     <React.Fragment>
       <SEO
@@ -39,19 +35,6 @@ function ProgramPage({ program }) {
                 </p>
               ) : null}
             </div>
-          </div>
-          <div className="justify-stretch mt-6 flex flex-col-reverse space-y-4 space-y-reverse sm:flex-row-reverse sm:justify-end sm:space-y-0 sm:space-x-3 sm:space-x-reverse md:mt-0 md:flex-row md:space-x-3">
-            <button
-              type="button"
-              onClick={() => router.back()}
-              className="inline-flex items-center justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-gray-100"
-            >
-              <ArrowNarrowLeftIcon
-                className="-ml-1 mr-2 h-5 w-5"
-                aria-hidden="true"
-              />
-              Back
-            </button>
           </div>
         </div>
         <div className="mx-auto mt-8 grid max-w-3xl grid-cols-1 gap-6 sm:px-6 lg:max-w-7xl lg:grid-flow-col-dense lg:grid-cols-3 lg:px-8">
