0f6145de3a4c5738b84576701e05e2944508cb97
Jonathan Steele <jonathan@steele.pro>
2022-09-27 18:31:25 +0100

    feat: Add <SpectatorTickets /> MDX component

--- components/mdx/index.js
+++ components/mdx/index.js
@@ -1,10 +1,52 @@
+import * as React from 'react'
+
 import Alert from '@/components/alert'
+import Button from '@/components/button'
+import { createCheckoutSession } from '@/lib/stripe'
 import PercentageRepWork from '@/components/mdx/percentage-rep-work'
 import PercentageWeightWork from '@/components/mdx/percentage-weight-work'
 import WorkoutBlock from '@/components/workout-block'
 
 const competitionMdxComponents = {
-  Alert: Alert
+  Alert: Alert,
+  SpectatorTickets: ({ competition, price, ...props }) => {
+    const [checkoutLoading, setCheckoutLoading] = React.useState(false)
+
+    const handleClick = async () => {
+      try {
+        setCheckoutLoading(true)
+
+        const checkoutSession = await createCheckoutSession({
+          adjustable_quantity: { enabled: true, minimum: 1, maximum: 10 },
+          allow_promotion_codes: false,
+          metadata: {
+            webhook_action: 'spectator'
+          },
+          mode: 'payment',
+          price
+        })
+
+        window.location.assign(checkoutSession.url)
+      } catch (error) {
+        console.error(error)
+      } finally {
+        setCheckoutLoading(false)
+      }
+    }
+
+    const pastCompetition = new Date(competition.endDate) < new Date()
+
+    return (
+      <Button
+        onClick={handleClick}
+        isLoading={checkoutLoading}
+        isDisabled={pastCompetition}
+        {...props}
+      >
+        Buy spectator tickets
+      </Button>
+    )
+  }
 }
 
 const programMdxComponents = {
